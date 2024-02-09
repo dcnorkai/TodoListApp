1272b3adaee95632e14b57509797aae6faa24862
Dominick Norkaitis <dcnorkai@buffalo.edu>
2024-02-09 11:21:26 -0500

    committing cause i know i'm about to break shit

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,7 +11,7 @@ namespace TodoListApp
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginPage());
+            MainPage = new NavigationPage(new MainPage());
         }
     }
 }
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+namespace TodoListApp.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        // Navigate to Login page
+        Navigation.PushAsync(new LoginPage());
+    }
+
+    private void OnRegisterClicked(object sender, EventArgs e)
+    {
+        // Navigate to user creation page
+        Navigation.PushAsync(new CreateUserPage());
+    }
+}
